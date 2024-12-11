fee26f1d26af9563a3ea368132ce050050543c45
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-12-10 19:41:45 -0600

    Setting up some initial values for the game panel

--- src/main/java/springrender/view/GamePanel.java
+++ src/main/java/springrender/view/GamePanel.java
@@ -3,4 +3,13 @@ package springrender.view;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
+
+    private static final int originalTileSize = 16;
+    private static final int scale = 3;
+    private int tileSize = originalTileSize * scale;
+    private int maxScreenWidthMulti = 16;
+    private int maxScreenHeightMulti = 12;
+    private int screenWidth = tileSize * maxScreenWidthMulti;
+    private int screenHeight = tileSize * maxScreenHeightMulti;
+
 }
