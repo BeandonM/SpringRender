febffd427be0b25a3453f0f8bbbe3f3f5bdea68c
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-12-10 19:27:44 -0600

    Added game window and save action config

--- src/main/java/springrender/view/GameWindow.java
+++ src/main/java/springrender/view/GameWindow.java
@@ -1,8 +1,21 @@
 package springrender.view;
 
+import javax.swing.*;
 
 public class GameWindow {
-    public GameWindow()
-    {
+
+    public GameWindow() {
+        JFrame gameWindow = new JFrame();
+        gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        gameWindow.setResizable(true);
+        gameWindow.setTitle("Test Title");
+
+        gameWindow.setLocationRelativeTo(null);
+        gameWindow.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        GameWindow gameWindow = new GameWindow();
     }
 }
