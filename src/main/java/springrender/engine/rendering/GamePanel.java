4607f86b887fc6fb1670dd351b51ff792497b578
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-12-11 19:31:05 -0600

    Using a delta time method for handling game loop

--- src/main/java/springrender/engine/rendering/GamePanel.java
+++ src/main/java/springrender/engine/rendering/GamePanel.java
@@ -44,30 +44,20 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
         double drawInterval = 1000000000 / 60;
-        double nextDrawTime = System.nanoTime() + drawInterval;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
 
         while (gameThread != null) {
 
-            //logger.info("Thread");
-            update();
-            repaint();
+            currentTime = System.nanoTime();
 
+            delta += (currentTime - lastTime) / drawInterval;
 
-            try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime /= 1000000;
-
-                if (remainingTime < 0) {
-                    remainingTime = 0;
-                }
-                Thread.sleep((long) remainingTime);
-
-                nextDrawTime += drawInterval;
-
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
             }
         }
     }
