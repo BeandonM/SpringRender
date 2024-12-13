781026610a8739b3f44acab702ba39bd6f44ba5d
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-12-13 15:46:33 -0600

    Initial implementation for Vector2D

---
+++ src/main/java/springrender/engine/core/Vector2D.java
@@ -0,0 +1,14 @@
+package springrender.engine.core;
+
+public class Vector2D {
+    private float positionX;
+    private float positionY;
+
+    public float getPositionX() {
+        return positionX;
+    }
+
+    public float getPositionY() {
+        return positionY;
+    }
+}
