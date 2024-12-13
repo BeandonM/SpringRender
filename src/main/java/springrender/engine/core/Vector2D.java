111dd6cdddc3c53e92e0ebd1208aaaad162e65d0
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-12-13 16:57:22 -0600

    Added some caching and some additional helper methods

--- src/main/java/springrender/engine/core/Vector2D.java
+++ src/main/java/springrender/engine/core/Vector2D.java
@@ -1,58 +1,151 @@
 package springrender.engine.core;
 
-public class Vector2D {
-    private float positionX;
-    private float positionY;
+import java.util.Objects;
 
+/**
+ * Represents a 2D vector with x and y components.
+ */
+public final class Vector2D {
+    private final float x;
+    private final float y;
+    private transient Float cachedMagnitude = -1f;
+    public static final Vector2D ZERO = new Vector2D(0f, 0f);
+    public static final Vector2D ONE = new Vector2D(1f, 1f);
+    public static final Vector2D UP = new Vector2D(0f, -1f);
+    public static final Vector2D DOWN = new Vector2D(0f, 1f);
+    public static final Vector2D LEFT = new Vector2D(-1f, 0f);
+    public static final Vector2D RIGHT = new Vector2D(1f, 0f);
 
+    /**
+     * Constructs a zero vector (0,0).
+     */
     public Vector2D() {
-        positionX = 0f;
-        positionY = 0f;
+        this.x = 0f;
+        this.y = 0f;
     }
 
+    /**
+     * Constructs a vector with specified x and y components.
+     *
+     * @param x The x-component.
+     * @param y The y-component.
+     */
     public Vector2D(float x, float y) {
-        positionX = x;
-        positionY = y;
+        this.x = x;
+        this.y = y;
     }
 
-    public float getPositionX() {
-        return positionX;
+    /**
+     * @return The x-component of the vector.
+     */
+    public float getX() {
+        return x;
     }
 
-    public float getPositionY() {
-        return positionY;
+    /**
+     * @return The y-component of the vector.
+     */
+    public float getY() {
+        return y;
     }
 
+    /**
+     * Calculates the magnitude of the vector.
+     *
+     * @return The magnitude.
+     */
     public float getMagnitude() {
-        return (float) Math.sqrt(positionX * positionX + positionY * positionY);
+        if (cachedMagnitude < 0f) {
+            cachedMagnitude = (float) Math.sqrt(x * x + y * y);
+        }
+        return cachedMagnitude;
     }
-    
+
+    /**
+     * Returns a normalized (unit length) vector.
+     *
+     * @return The normalized vector.
+     */
+    public Vector2D normalize() {
+        float magnitude = getMagnitude();
+        if (magnitude < 1e-6f) return ZERO;
+        return new Vector2D(x / magnitude, y / magnitude);
+    }
+
+    /**
+     * Adds this vector to another vector.
+     *
+     * @param other The vector to add.
+     * @return A new Vector2D instance representing the sum.
+     */
+    public Vector2D add(Vector2D other) {
+        return new Vector2D(this.x + other.x, this.y + other.y);
+    }
+
+    /**
+     * Subtracts another vector from this vector.
+     *
+     * @param other The vector to subtract.
+     * @return A new Vector2D instance representing the difference.
+     */
+    public Vector2D subtract(Vector2D other) {
+        return new Vector2D(this.x - other.x, this.y - other.y);
+    }
+
+    /**
+     * Calculates the dot product of this vector with another.
+     *
+     * @param other The other vector.
+     * @return The dot product.
+     */
+    public float dotProduct(Vector2D other) {
+        return this.x * other.x + this.y * other.y;
+    }
+
+    /**
+     * Multiplies this vector by a scalar.
+     *
+     * @param scalar The scalar to multiply by.
+     * @return A new Vector2D instance representing the scaled vector.
+     */
+    public Vector2D multiply(float scalar) {
+        return new Vector2D(this.x * scalar, this.y * scalar);
+    }
+
+    /**
+     * Divides this vector by a scalar.
+     *
+     * @param scalar The scalar to divide by.
+     * @return A new Vector2D instance representing the scaled vector.
+     * @throws IllegalArgumentException If scalar is zero.
+     */
+    public Vector2D divide(float scalar) {
+        if (scalar == 0) throw new IllegalArgumentException("Cannot divide by zero.");
+        return new Vector2D(this.x / scalar, this.y / scalar);
+    }
+
+    public float angleBetween(Vector2D other) {
+        float dot = this.dotProduct(other);
+        float mags = this.getMagnitude() * other.getMagnitude();
+        return (float) Math.acos(dot / mags);
+    }
+
     @Override
     public String toString() {
-        return "(" + positionX + ", " + positionY + ")";
+        return "(" + x + ", " + y + ")";
     }
 
-    public static Vector2D zeroVector() {
-        return new Vector2D(0f, 0f);
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Vector2D)) return false;
+        Vector2D other = (Vector2D) obj;
+        return Float.compare(x, other.x) == 0 &&
+                Float.compare(y, other.y) == 0;
     }
 
-    public static Vector2D oneVector() {
-        return new Vector2D(1f, 1f);
-    }
-
-    public static Vector2D upVector() {
-        return new Vector2D(0f, -1f);
-    }
-
-    public static Vector2D downVector() {
-        return new Vector2D(0f, 1f);
-    }
-
-    public static Vector2D leftVector() {
-        return new Vector2D(-1f, 0f);
-    }
-
-    public static Vector2D rightVector() {
-        return new Vector2D(1f, 0f);
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
