564993b3ae62f671bcce531237ccfd6c9e51be18
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-12-14 16:15:46 -0600

    Initial implementation for GameSingleThreaded

---
+++ src/main/java/springrender/engine/core/GameSingleThreaded.java
@@ -0,0 +1,29 @@
+package springrender.engine.core;
+
+public class GameSingleThreaded implements GameThread {
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
