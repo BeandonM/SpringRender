ed2b4f9eb3623fd18ed0af5c6ef0ee6414726a1e
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-12-14 16:12:58 -0600

    Finished implementation for GameThread

--- src/main/java/springrender/engine/core/GameThread.java
+++ src/main/java/springrender/engine/core/GameThread.java
@@ -4,4 +4,8 @@ public interface GameThread extends Runnable {
     public abstract void start();
 
     public abstract void stop();
+
+    public abstract void update();
+
+    public abstract void render();
 }
