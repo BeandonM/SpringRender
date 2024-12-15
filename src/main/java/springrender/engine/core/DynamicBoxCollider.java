6fb27489ee5242901b8caf93e67ddd9c2317cb66
Brandon Maweu <53877311+BeandonM@users.noreply.github.com>
2024-12-15 15:21:09 -0600

    Updated DynamicBoxCollider and implemented the collision methods

--- src/main/java/springrender/engine/core/DynamicBoxCollider.java
+++ src/main/java/springrender/engine/core/DynamicBoxCollider.java
@@ -1,26 +1,63 @@
 package springrender.engine.core;
 
+import java.awt.Rectangle;
 import java.awt.Shape;
 
-public class DynamicBoxCollider implements DynamicCollider {
+public class DynamicBoxCollider implements DynamicCollider, Transformable {
+
+    private Transform transform;
+    private int width;
+    private int height;
+
+    public DynamicBoxCollider(Transform transform, int width, int height) {
+        this.transform = transform;
+        this.width = width;
+        this.height = height;
+    }
 
     @Override
     public Shape getBoundingBox() {
-        return null;
+        return new Rectangle(
+                (int) transform.getPosition().getX(),
+                (int) transform.getPosition().getY(),
+                width,
+                height
+        );
     }
 
     @Override
     public boolean isColliding(Collider other) {
-        return false;
+        //Basic implementation, just converts the shape to a rect, I do plan on adding intersections between other shapes
+        return getBoundingBox().getBounds2D().intersects(other.getBoundingBox().getBounds2D());
+        //TODO Handle intersections with other shapes
+        /**
+         * Some starting implementation I have.
+         * if( getBoundingBox().getBounds().intersects(other.getBoundingBox().getBounds()) ){
+         *     Area a = new Area(myShape);
+         *     a.intersect(new Area(otherShape));
+         *     return !a.isEmpty();
+         * }
+         */
+        //if (getBoundingBox().getBounds().intersects(other.getBounds())) {
+        //     return getBoundingBox().intersects(other.getBoundingBox())
+        // }
     }
 
     @Override
     public void onCollisionEnter(Collider other) {
-        DynamicCollider.super.onCollisionEnter(other);
+        System.out.println("Collision started with: " + other);
+        //DynamicCollider.super.onCollisionEnter(other);
     }
 
     @Override
     public void onCollisionExit(Collider other) {
-        DynamicCollider.super.onCollisionExit(other);
+
+        System.out.println("Collision ended with: " + other);
+        //DynamicCollider.super.onCollisionExit(other);
+    }
+
+    @Override
+    public Transform getTransform() {
+        return transform;
     }
 }
